d55a504bf698a7328ef10a822023e233adbaafb7
Chiara Kenagy <chiara.kenagy.22@bishops.com>
2021-05-05 09:12:46 -0700

    Set up new account registration/login

---
+++ finalproject/mysite/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+# accounts/views.py
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy(' ')
+    template_name = 'polls/signup.html'
+
+
+# Create your views here.
--- finalproject/mysite/mysite/urls.py
+++ finalproject/mysite/mysite/urls.py
@@ -20,5 +20,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')), # new
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='polls/user.html'), name='home'), # new
+    #path('accounts/', include('django.contrib.auth.urls')),
 ]
