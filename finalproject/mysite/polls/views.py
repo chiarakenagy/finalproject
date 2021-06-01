59f1566a74db99d66921bda58e6536a7099e426e
Chiara Kenagy <chiara.kenagy.22@bishops.com>
2021-06-01 15:05:29 -0700

    completed voting system

--- finalproject/mysite/polls/views.py
+++ finalproject/mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
 from django.template import loader
@@ -31,6 +31,23 @@ class IndexView(View):
                 logout(request)
         loggedIn = request.user.is_authenticated
 
+        if "question" in request.POST.keys():
+            question = Question(
+                question_text = request.POST["question"],
+                pub_date = timezone.now(),
+            )
+            question.save()
+            choice1 = Choice(
+                question = question,
+                choice_text = request.POST["choice1"],
+            )
+            choice2 = Choice(
+                question = question,
+                choice_text = request.POST["choice2"],
+            )
+            choice1.save()
+            choice2.save()
+
         latest_question_list = Question.objects.order_by('-pub_date')[:5]
         template = loader.get_template('polls/index.html')
         context = {
@@ -41,31 +58,51 @@ class IndexView(View):
 
         output = ', '.join([q.question_text for q in latest_question_list])
         return HttpResponse(template.render(context, request))
-        # can treat the def post differently - coming from a form - welcome message, check if user is logged in
 
 
-class DetailView(View):
-    def get(self, request):
-        return HttpResponse("Your're looking at question %s." % question_id)
+
+# class DetailView(View):
+#     def get(self, request, question_id):
+#         return HttpResponse("Your're looking at question %s." % question_id)
 
 class ResultsView(View):
-    def get(self, request):
+    def get(self, request, question_id):
         response = "Your're looking at the results of question %s."
         return HttpResponse(response % question_id)
 
-class VotingView(View):
-    def get(self, request):
+class DetailView(View):
+    def get(self, request, question_id):
         question = get_object_or_404(Question, pk = question_id)
         try:
-            selected_choice = question.choice_set.get(pk = request.POST['choice'])
+            choices = question.choice_set.filter(question=question)
         except (KeyError, Choice.DoesNotExist):
             # Redisplay the question voting form.
-            return render(request, 'polls / index.html', {
+            return render(request, 'polls/detail.html', {
                 'question': question,
                 'error_message': "You didn't select a choice.",
                 'user': request.user,
             })
         else:
-            selected_choice.votes += 1
-            selected_choice.save()
-            return HttpResponseRedirect(reverse('polls:results', args =(question.id, )))
+            #selected_choice.votes += 1
+            #selected_choice.save()
+            pass
+
+        context= { 'question': question,
+                'choices': choices,
+
+
+        }
+        return render(request, "polls/detail.html", context)
+    def post(self, request, question_id):
+        if "pollChoices" in request.POST:
+            choice = Choice.objects.get(id=int(request.POST['pollChoices']))
+            question = choice.question
+            choice.votes += 1
+            choice.save()
+
+            choices = Choice.objects.filter(question=question)
+
+            context = {'choices': choices,
+                        'question': question,
+                        }
+        return render(request, 'polls/results.html', context)
